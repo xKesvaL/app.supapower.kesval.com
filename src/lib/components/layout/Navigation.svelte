2cccd315915797222c4c13da05e79a8aa2a8cf23
KesvaL <jabeddou@gmail.com>
2023-09-05 09:40:46 +0200

    feat: navigation styling (mobile, pc)

--- src/lib/components/layout/Navigation.svelte
+++ src/lib/components/layout/Navigation.svelte
@@ -9,16 +9,22 @@
 
 <nav>
 	<a href="/" class:active={$page.url.pathname === '/'}>
-		<IconHouse />
-		<span class="visually-hidden">{$t('pages.home.label')}</span>
+		<div class="icon">
+			<IconHouse />
+		</div>
+		<span>{$t('pages.home.label')}</span>
 	</a>
 	<a href="/workout" class:active={$page.url.pathname.startsWith('/workout')}>
-		<IconBarbell />
-		<span class="visually-hidden">{$t('pages.workout.label')}</span>
+		<div class="icon">
+			<IconBarbell />
+		</div>
+		<span>{$t('pages.workout.label')}</span>
 	</a>
 	<a href="/profile" class:active={$page.url.pathname.startsWith('/profile')}>
-		<IconUser />
-		<span class="visually-hidden">{$t('pages.profile.label')}</span>
+		<div class="icon">
+			<IconUser />
+		</div>
+		<span>{$t('pages.profile.label')}</span>
 	</a>
 </nav>
 
@@ -30,28 +36,101 @@
 		left: 0;
 		right: 0;
 		height: 3rem;
-		background: red;
-		padding: 0.5rem 1rem;
+		background: rgba(var(--base-200-rgb), 0.75);
+		box-shadow: 0 -1px 4px rgba(var(--base-200-rgb), 0.4);
 
 		display: flex;
 		align-items: center;
 		justify-content: space-around;
 
+		@include mq(lg) {
+			position: sticky;
+			right: auto;
+			bottom: auto;
+			top: 0;
+			height: 100vh;
+			width: 12rem;
+
+			flex-direction: column;
+			justify-content: flex-start;
+			align-items: flex-start;
+		}
+
 		a {
-			width: 28px;
-			height: 28px;
+			height: 100%;
 			display: flex;
 			align-items: center;
 			justify-content: center;
 			position: relative;
+			flex: 1;
 
-			&.active::before {
-				view-transition-name: navigation-active;
+			@include mq(lg) {
+				width: 100%;
+				gap: 0.75rem;
+				padding: 1.5rem 1.5rem;
+				height: auto;
+				flex: 0;
+
+				justify-content: flex-start;
+			}
+
+			:global(svg) {
+				transition: 0.2s ease;
+			}
+
+			&.active {
+				color: var(--primary-700);
+
+				:global(svg) {
+					fill: var(--primary-700);
+				}
+
+				&::before {
+					view-transition-name: navigation-active;
+					position: absolute;
+					content: '';
+					width: 100%;
+					height: 2px;
+					top: 0;
+					left: 0;
+					right: 0;
+					background: var(--primary-500);
+
+					@include mq(md) {
+						right: auto;
+						bottom: 0;
+						height: 100%;
+						width: 2px;
+					}
+				}
+			}
+
+			.icon {
+				width: 1.5rem;
+				height: 1.5rem;
+				flex: 0 0 auto;
+			}
+
+			span {
 				position: absolute;
-				content: '';
-				width: 20px;
-				height: 20px;
-				background: blue;
+				width: 1px;
+				height: 1px;
+				padding: 0;
+				margin: -1px;
+				overflow: hidden;
+				clip: rect(0, 0, 0, 0);
+				white-space: nowrap;
+				border: 0;
+
+				@include mq(lg) {
+					position: relative;
+					width: auto;
+					height: auto;
+					margin: 0;
+					clip: auto;
+					white-space: nowrap;
+					overflow: visible;
+				}
 			}
 		}
 	}
