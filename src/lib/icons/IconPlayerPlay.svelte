59eeaff0e6263307b20440c13a3d4acd87af54d4
KesvaL <47763141+xKesvaL@users.noreply.github.com>
2023-09-20 09:22:48 +0000

    feat: new player icon for workout resume

---
+++ src/lib/icons/IconPlayerPlay.svelte
@@ -0,0 +1,14 @@
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	width="100%"
+	height="100%"
+	viewBox="0 0 24 24"
+	stroke-width="1.5"
+	stroke="currentColor"
+	fill="none"
+	stroke-linecap="round"
+	stroke-linejoin="round"
+>
+	<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+	<path d="M7 4v16l13 -8z"></path>
+</svg>
