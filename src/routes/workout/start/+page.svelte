f5cfefeef9c7b229881e0fe6d7fec7a065cde72a
KesvaL <47763141+xKesvaL@users.noreply.github.com>
2023-09-20 09:58:36 +0000

    feat: workout start page infos

--- src/routes/workout/start/+page.svelte
+++ src/routes/workout/start/+page.svelte
@@ -1,15 +1,13 @@
 <script lang="ts">
 	import WorkoutStartHeader from '$lib/containers/workout/start/WorkoutStartHeader.svelte';
-	import type { UserDataStore } from '$lib/firebase/user/types';
-	import { getContext } from 'svelte';
 	import type { LayoutData } from '../../$types';
+	import WorkoutStartStats from '$lib/containers/workout/start/WorkoutStartStats.svelte';
 
 	export let data: LayoutData;
-
-	const userData: UserDataStore = getContext('userData');
 
 	$: ({ fRel } = data);
 </script>
 
 <WorkoutStartHeader {fRel} />
+<WorkoutStartStats />
 <section class="container">ok</section>
