f0640ef4ebbcf0d1377651a06b76e16b0d05e383
KesvaL <47763141+xKesvaL@users.noreply.github.com>
2023-09-20 09:22:26 +0000

    feat: workout logging header style

--- src/lib/containers/workout/start/WorkoutStartHeader.svelte
+++ src/lib/containers/workout/start/WorkoutStartHeader.svelte
@@ -5,32 +5,54 @@
 	export let fRel: string;
 </script>
 
-<header class="page-header">
-	<div class="start">
-		<a class="button icon" href={fRel}>
-			<IconChevronDown />
-		</a>
-		<span>{$t('pages.workout.label')}</span>
-	</div>
-	<div></div>
-	<div class="end">
-		<button class="primary"> Finish </button>
-	</div>
-</header>
+<div class="wrapper">
+	<header class="page-header">
+		<div class="start">
+			<a class="button icon" href={fRel}>
+				<IconChevronDown />
+			</a>
+			<span>{$t('pages.workout.label')}</span>
+		</div>
+		<div></div>
+		<div class="end">
+			<button class="primary"> Finish </button>
+		</div>
+	</header>
+	<progress value="50" max="100" class="primary"></progress>
+</div>
 
 <style lang="scss">
-	header {
-		.start {
-			.icon {
-				width: 40px;
-				height: 40px;
-				padding: 8px;
-				background: none;
-			}
+	.wrapper {
+		display: flex;
+		flex-direction: column;
+		position: sticky;
+		top: 0;
+		left: 0;
+		right: 0;
+		padding: 0;
 
-			gap: 0.5rem;
-			font-size: var(--fs-500);
-			font-weight: 600;
+		header {
+			background: rgba(var(--base-200-rgb), 1);
+
+			.start {
+				.icon {
+					width: 40px;
+					height: 40px;
+					padding: 8px;
+					background: none;
+				}
+
+				gap: 0.5rem;
+				font-size: var(--fs-500);
+				font-weight: 600;
+			}
+		}
+
+		progress {
+			width: 100%;
+			height: 0.25rem;
+			border: none;
+			border-radius: 0;
 		}
 	}
 </style>
