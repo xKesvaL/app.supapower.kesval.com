3020f4c2e4167885ae5998f12717d41f328455a0
xKesvaL <jabeddou@gmail.com>
2023-09-24 18:20:10 +0200

    feat: new overlay component

---
+++ src/lib/components/layout/Overlay.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { blur } from 'svelte/transition';
+
+	export let strength = 0.8;
+</script>
+
+<div class="overlay" transition:blur={{ amount: 5, duration: 300 }} style="--ov-str: {strength}" />
+
+<style lang="scss">
+	.overlay {
+		view-transition-name: overlay;
+		position: fixed;
+		inset: 0;
+		z-index: 1000;
+
+		background: rgba(var(--base-100-rgb), var(--ov-str, 0.8));
+		backdrop-filter: blur(5px);
+	}
+</style>
