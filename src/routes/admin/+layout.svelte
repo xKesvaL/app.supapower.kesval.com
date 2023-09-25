61ca291f55c9895ac222462c9b525bdd3c7eb17a
xKesvaL <jabeddou@gmail.com>
2023-09-25 22:32:35 +0200

    feat: make admin route private

---
+++ src/routes/admin/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { ROUTES } from '$lib/config';
+	import type { UserDataStoreContext } from '$lib/firebase/user/types';
+	import { getContext } from 'svelte';
+
+	const userData: UserDataStoreContext = getContext('userData');
+
+	$: $userData.type !== 'admin' && goto(ROUTES.home);
+</script>
+
+{#if $userData.type === 'admin'}
+	<slot />
+{:else}
+	<p>Not authorized</p>
+{/if}
