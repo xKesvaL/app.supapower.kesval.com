a5678e259a7350c4709d3f265a8f8c7103e50b36
KesvaL <jabeddou@gmail.com>
2023-10-26 13:32:58 +0200

    feat: show sets in stats

--- src/lib/components/exercises/data/ExerciseDataReps.svelte
+++ src/lib/components/exercises/data/ExerciseDataReps.svelte
@@ -4,4 +4,4 @@
 	export let reps: number | null;
 </script>
 
-<Input bind:value={reps} type="number" class="text-center p-2" />
+<Input bind:value={reps} type="number" class="text-center p-2 border-muted" />
